6ddae3e7066d60d15f19512846c3f5e6262ac68a
mihirprakash <mp107@rice.edu>
2025-01-26 14:31:37 -0600

    Added iterative sol to BS

--- 4.Binary_Search/lc704_Binary_Search.cpp
+++ 4.Binary_Search/lc704_Binary_Search.cpp
@@ -13,26 +13,42 @@
 
 using namespace std;
 //Recursive Solution
+// class Solution {
+// public:
+//     int searchHelper(vector<int>& nums, int target, int s, int e){
+//         int mid = s+(e-s)/2;
+//         //base case
+//         if(s>e)return -1;
+//         if(nums[mid]==target)return mid;
+
+//         if(nums[mid]>target){
+//             //search in left half
+//             return searchHelper(nums, target, s, mid-1);
+//         }else{
+//             //search in right half
+//             return searchHelper(nums, target, mid+1, e);
+//         }
+//     }
+//     int search(vector<int>& nums, int target) {
+//         int n= nums.size();
+//         return searchHelper(nums, target, 0, n-1);
+//     }
+// };
+
+//Iterative Solution
 class Solution {
 public:
-    int searchHelper(vector<int>& nums, int target, int s, int e){
-        int mid = s+(e-s)/2;
-        //base case
-        if(s>e)return -1;
-        if(nums[mid]==target)return mid;
-
-        if(nums[mid]>target){
-            //search in left half
-            return searchHelper(nums, target, s, mid-1);
-        }else{
-            //search in right half
-            return searchHelper(nums, target, mid+1, e);
-        }
-    }
     int search(vector<int>& nums, int target) {
-        int n= nums.size();
-        return searchHelper(nums, target, 0, n-1);
-    }
+        int n=nums.size();
+        int s = 0, e = n-1;
+        while(s<=e){
+            int mid = s+(e-s)/2;
+            if(nums[mid]==target)return mid;
+            else if(nums[mid]<target)s = mid+1;
+            else e = mid-1;
+        }
+        return -1;
+    }   
 };
 int main() {
     cout << "Hello, World!" << endl;
