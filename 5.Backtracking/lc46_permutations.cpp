cd7c281628db9e6711107fb7c8a6c53e44f06f9a
mihirprakash <mp107@rice.edu>
2025-01-29 17:47:41 -0600

    Solved permutations by swap method

--- 5.Backtracking/lc46_permutations.cpp
+++ 5.Backtracking/lc46_permutations.cpp
@@ -48,6 +48,29 @@ we are building curr by adding ith element
 we are then recursively adding other non used element
 before moving with other element to build curr ,  we backtrack
 */
+
+
+//Method-2
+//  void permuteHelper(vector<int>& nums, int i){
+//         // I trust that this function will permute from i+1 till end
+//         //till 0 to i i will fix the elements
+        
+//         if(i==nums.size()){
+//             ans.push_back(nums);
+//             return;
+//         }
+//         for(int j=i; j<nums.size(); j++){
+//             swap(nums[i], nums[j]); //now ith position is fixed
+//             permuteHelper(nums, i+1);
+//             swap(nums[i], nums[j]); //undo the change/ backtrack
+
+             
+//         }
+//     }
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         permuteHelper(nums, 0);
+//         return ans;
+//     }
 int main() {
     cout << "Hello, World!" << endl;
     return 0;
