944392c6b65e3906ff115878a1b8a8b93a7a5d03
mihirprakash <mp107@rice.edu>
2025-02-03 15:58:33 -0600

    Solved post order traversal

--- 6.Trees/lc145_postorder_traversal.cpp
+++ 6.Trees/lc145_postorder_traversal.cpp
@@ -11,6 +11,18 @@
 #include <cstdlib>
 #include <ctime>
 
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 struct TreeNode {
       int val;
       TreeNode *left;
@@ -21,24 +33,21 @@ struct TreeNode {
  };
 class Solution {
 public:
-    void preorderTraversal(TreeNode* root, vector<int>&res){
+    void postorderTraversal(TreeNode* root, vector<int>&res){
+        //I trust that this function will do a Post order traversal of the tree rooted at root
         //base case
         if(root==NULL)return;
-
+        postorderTraversal(root->left, res);
+        postorderTraversal(root->right, res);
         res.push_back(root->val);
-        preorderTraversal(root->left, res);
-        preorderTraversal(root->right, res);
 
     }
-    vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> postorderTraversal(TreeNode* root) {
         vector<int>res;
-        preorderTraversal(root, res);
+        postorderTraversal(root, res);
         return res;
     }
-    
 };
-using namespace std;
-
 int main() {
     cout << "Hello, World!" << endl;
     return 0;
