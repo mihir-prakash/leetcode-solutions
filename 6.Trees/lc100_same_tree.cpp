48d490f43bdaae3a076f1fa2d1b7a97a196ef01f
mihirprakash <mp107@rice.edu>
2025-02-09 11:25:03 -0600

    Solved same tree

--- 6.Trees/lc100_same_tree.cpp
+++ 6.Trees/lc100_same_tree.cpp
@@ -13,7 +13,6 @@
 #include <ctime>
 
 using namespace std;
-
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -25,7 +24,6 @@ using namespace std;
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 struct TreeNode {
     int val;
     TreeNode *left;
@@ -33,34 +31,18 @@ struct TreeNode {
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
 class Solution {
 public:
-    TreeNode* invertTreeHelper(TreeNode* &root){
-        //base case
-        if(root==NULL)return NULL;
-        //do a dfs and for each node swap its lc and rc. I trust this recursvie function to swap tree rooted at root
-        TreeNode* left = invertTreeHelper(root->left);
-        TreeNode* right = invertTreeHelper(root->right);
-        //swap lst & rst
-        root->left = right;
-        root->right = left;
-
-        return root;
-
+    bool isSameTreeHelper(TreeNode* &p, TreeNode* &q) {
+        //do a dfs and check for each node ->compare val of both. if not same return false else keep traversing.
+        if(p==NULL && q==NULL)return true; //if both are null
+        if(!p || !q)return false; //if one is null and one is not
+        
+        if(p->val != q->val)return false;      //if the values are not same
+        return isSameTreeHelper(p->left, q->left) && isSameTreeHelper(p->right, q->right);
     }
-    // TreeNode* invertTreeHelper(TreeNode* &root){
-    //     //base case
-    //     if(root==NULL)return NULL;
-    //     //do a dfs and for each node swap its lc and rc. 
-    //     invertTreeHelper(root->left);
-    //     invertTreeHelper(root->right);
-    //     swap(root->left, root->right);
-    //     return root;
-
-    // }
-    TreeNode* invertTree(TreeNode* root) {
-        return invertTreeHelper(root);
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        return isSameTreeHelper(p,q);
     }
 };
 int main() {
